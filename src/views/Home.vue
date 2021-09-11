8929505633eb9b3b565ad308266bef2bb08013f5
callmesoul <547437716@qq.com>
2021-09-11 21:44:19 +0800

    屏蔽有问题的账号

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -118,6 +118,7 @@ import IsNull from '../components/IsNull/IsNull.vue'
 import { classifyList } from '@/config'
 import { useI18n } from 'vue-i18n'
 import { setDataStrclassify } from '@/utils/util'
+import SetHomeDatas from '@/utils/homeSetData'
 
 const i18n = useI18n()
 const store = useStore()
@@ -150,35 +151,8 @@ async function getNftList(isCover: boolean = false) {
   if (res.code === NftApiCode.success) {
     if (isCover) Nfts.length = 0
     if (res.data.results.items.length > 0) {
-      res.data.results.items.map((item: GetNftIssueyTxIdResItem) => {
-        if (item.nftIssueMetaId.slice(0, 6) !== '0064d4') {
-          if (
-            item.nftIssuer.toLowerCase().indexOf('showpayteam') !== -1 &&
-            item.nftIssueMetaId !==
-              '974e2977d5c9446f7f48fd82c9ea51f82749b9ef7c00d26b73bc450d167d5f31'
-          ) {
-            const data =
-              item.nftDataStr && item.nftDataStr !== '' ? JSON.parse(item.nftDataStr) : null
-            const classify = setDataStrclassify(data)
-            Nfts.push({
-              name: item.nftName ? item.nftName : '--',
-              amount: item.nftPrice,
-              foundryName: item.nftIssuer,
-              classify: classify,
-              head: '',
-              tokenId: item.nftGenesis + item.nftTokenIndex,
-              coverUrl: item.nftIcon,
-              putAway: item.nftIsReady,
-              metaId: item.nftIssueMetaId,
-              productName: item.nftName,
-              deadlineTime: 0,
-              genesis: item.nftGenesis,
-              tokenIndex: item.nftTokenIndex,
-              codehash: item.nftCodehash,
-            })
-          }
-        }
-      })
+      const results = await SetHomeDatas(res.data.results.items)
+      Nfts.push(...results)
     } else {
       pagination.nothing = true
     }
@@ -199,35 +173,8 @@ function getRecommendNftList() {
     })
     if (res.code === 0) {
       if (res.data.results.items.length > 0) {
-        res.data.results.items.map((item) => {
-          if (item.nftIssueMetaId.slice(0, 6) !== '0064d4') {
-            if (
-              item.nftIssuer.toLowerCase().indexOf('showpayteam') !== -1 &&
-              item.nftIssueMetaId !==
-                '974e2977d5c9446f7f48fd82c9ea51f82749b9ef7c00d26b73bc450d167d5f31'
-            ) {
-              const data =
-                item.nftDataStr && item.nftDataStr !== '' ? JSON.parse(item.nftDataStr) : null
-              const classify = setDataStrclassify(data)
-              recommendNfts.push({
-                name: item.nftName ? item.nftName : '--',
-                amount: item.nftPrice,
-                foundryName: item.nftIssuer,
-                classify: classify,
-                head: '',
-                tokenId: item.nftGenesis + item.nftTokenIndex,
-                coverUrl: item.nftIcon,
-                putAway: item.nftIsReady,
-                metaId: item.nftIssueMetaId,
-                productName: item.nftName,
-                deadlineTime: 0,
-                genesis: item.nftGenesis,
-                tokenIndex: item.nftTokenIndex,
-                codehash: item.nftCodehash,
-              })
-            }
-          }
-        })
+        const results = await SetHomeDatas(res.data.results.items)
+        Nfts.push(...results)
       }
       isShowRecommendSkeleton.value = false
     }
