2b9e1b67ba06dda1a5bb6808ad90535bb4669983
chenyibin <cheyibin@showpay.top>
2021-10-21 17:24:18 +0800

    add app alert

--- src/utils/sdk.ts
+++ src/utils/sdk.ts
@@ -899,13 +899,13 @@ export default class Sdk {
         // @ts-ignore
         window[functionName] = callback
         if (window.appMetaIdJsV2) {
-          window.appMetaIdJsV2?.signMessage(
+          window.appMetaIdJsV2?.signMessageApp(
             store.state.token!.access_token,
             JSON.stringify(params),
             functionName
           )
         } else {
-          window.appMetaIdJs?.signMessage(
+          window.appMetaIdJs?.signMessageApp(
             store.state.token!.access_token,
             JSON.stringify(params),
             functionName
