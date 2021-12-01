0821b49343588267fe982181cc91f4731db2d9fc
CallMeSoul <callmesoul@foxmail.com>
2021-12-01 11:08:36 +0800

    update:抽离首页nft介绍模块为模板

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -65,73 +65,8 @@
       </NftSkeleton>
     </div>
 
-    <!-- nft 模块 -->
-    <div class="nft-muodle">
-      <div class="container">
-        <div class="title">{{ $t('nftModuleTitle') }}</div>
-        <div class="nft-module-list flex ">
-          <!-- 海量应用场景 -->
-          <div class="nft-module-item flex1">
-            <div class="nft-module-item-warp">
-              <div class="icon">
-                <img src="@/assets/images/img_2.svg" />
-              </div>
-              <div class="name">{{ $t('nftModuleName1') }}</div>
-              <div class="drsc">
-                <div>
-                  {{ $t('nftModuleDrsc1') }}
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <!-- 极低的铸造费用 -->
-          <div class="nft-module-item flex1">
-            <div class="nft-module-item-warp">
-              <div class="icon">
-                <img src="@/assets/images/img_1.svg" />
-              </div>
-              <div class="name">{{ $t('nftModuleName2') }}</div>
-              <div class="drsc">
-                <div>
-                  {{ $t('nftModuleDrsc2') }}
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <!-- 覆盖全网用户 -->
-          <div class="nft-module-item flex1">
-            <div class="nft-module-item-warp">
-              <div class="icon">
-                <img src="@/assets/images/img_4.svg" />
-              </div>
-              <div class="name">{{ $t('nftModuleName3') }}</div>
-              <div class="drsc">
-                <div>
-                  {{ $t('nftModuleDrsc3') }}
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <!-- 创作者认证 -->
-          <div class="nft-module-item flex1">
-            <div class="nft-module-item-warp">
-              <div class="icon">
-                <img src="@/assets/images/img_3.svg" />
-              </div>
-              <div class="name">{{ $t('nftModuleName4') }}</div>
-              <div class="drsc">
-                <div>
-                  {{ $t('nftModuleDrsc4') }}
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <!-- nft 介绍模块 -->
+    <HomeNFTIntro />
 
     <!-- 所有类别 -->
     <div class="section container">
@@ -163,6 +98,7 @@ import {
   NftApiCode,
 } from '@/api'
 import NftItem from '@/components/Nft-item/Nft-item.vue'
+import HomeNFTIntro from '@/template/HomeNFTIntro/HomeNFTIntro.vue'
 import NftSkeleton from '@/components/NftSkeleton/NftSkeleton.vue'
 import { useStore } from '@/store'
 import { reactive, ref } from 'vue'
