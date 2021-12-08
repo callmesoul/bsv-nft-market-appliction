77f0aa7f16735da0455cc721ba5187667ba196c8
CallMeSoul <callmesoul@foxmail.com>
2021-12-08 18:00:14 +0800

    init cert user ingo

--- src/components/Cert/Cert.vue
+++ src/components/Cert/Cert.vue
@@ -1,16 +1,27 @@
 <template>
-  <div class="cert flex flex-pack-end" @click="toCert">
-    <a class="flex flex-align-center flex-pack-end">
-      <img v-if="props.metaId && isCert" class="icon-cert" src="@/assets/images/icon_cer.svg" />
-      <span>{{ (props.metaId && isCert) || certed ? $t('isCert') : $t('unCert') }}</span>
-      <i class="el-icon-arrow-right icon-right"></i>
-    </a>
+  <div class="cert flex flex-pack-end">
+    <ElPopover placement="right" :width="400" trigger="click">
+      <template #reference>
+        <a class="flex flex-align-center flex-pack-end">
+          <img v-if="props.metaId && isCert" class="icon-cert" src="@/assets/images/icon_cer.svg" />
+          <span>{{ (props.metaId && isCert) || certed ? $t('isCert') : $t('unCert') }}</span>
+          <i class="el-icon-arrow-right icon-right"></i>
+        </a>
+      </template>
+      <div class="certed-user-info">
+        <div class="certed-user-item flex flex-align-center">
+          <span class="label">{{ $t('realName') }}:</span><span class="value flex1"></span>
+          <span class="label">{{ $t('idNumber') }}:</span><span class="value flex1"></span>
+          <span class="label">{{ $t('certBody') }}:</span><span class="value flex1"></span>
+        </div>
+      </div>
+    </ElPopover>
   </div>
 </template>
 <script lang="ts" setup>
 import { ref, computed } from '@vue/runtime-core'
 import { certedMetaIds } from '@/config'
-import { ElMessage } from 'element-plus'
+import { ElMessage, ElPopover } from 'element-plus'
 import { useI18n } from 'vue-i18n'
 import { useStore } from '@/store'
 const store = useStore()
@@ -19,7 +30,6 @@ const props = defineProps<{
   certed?: boolean
 }>()
 const isCert = computed(() => {
-  debugger
   return props.metaId && store.state.isCertedMetaIds.find(item => item === props.metaId)
 })
 const i18n = useI18n()
