78b42cbb3ea0279f1fc641d4a3c2d694dfde18b6
CallMeSoul <callmesoul@foxmail.com>
2021-12-20 10:16:40 +0800

    fix: 从专辑列表进入专辑页面，专辑页面不是显示最顶部的NFT，而是显示页面中间部分的NFT

--- src/router.ts
+++ src/router.ts
@@ -129,7 +129,8 @@ export const router = createRouter({
       return savedPosition
     } else {
       // TODO: check if parent in common that works with alias
-      if (to.matched.every((record, i) => from.matched[i] !== record)) return { left: 0, top: 0 }
+      // if (to.matched.every((record, i) => from.matched[i] !== record)) return { left: 0, top: 0 }
+      return { left: 0, top: 0 }
     }
     // leave scroll as it is by not returning anything
     // https://github.com/Microsoft/TypeScript/issues/18319
